64276b35d8d27a57772eb1327cb9a9bf2580a115
DaveedC-ops <62561428+DaveedC-ops@users.noreply.github.com>
2020-03-23 11:15:31 -0700

    Create master mind.py

---
+++ master mind.py
@@ -0,0 +1,52 @@
+password = []
+guess = []
+close = 0
+
+def create_password():
+    password.append(1)
+    password.append(2)
+    password.append(3)
+    password.append(4)
+    
+def get_guess():
+    temp = []
+    for i in range(4): #[0, 1, 2, 3]
+        word = input("Give me a number fool!") # "12"
+        num = int(word)
+        temp.append(num)
+    return temp    
+    
+def is_correct(guess):
+    global close
+    
+    flag = True
+    close = 0
+    
+    for i in range(4):
+        if guess[i] != password[i]:
+            flag = False
+            
+            if guess[i] in password:
+                close += 1
+           
+            print ("_")
+            
+        else:
+            print(guess[i])
+            
+    return flag    
+    
+def main():
+    # Create the password first
+    create_password()
+    
+    guess = get_guess()
+    while is_correct(guess) == False:
+        print("You got it wrong!")
+        guess = get_guess()
+        
+    print("Correct!!!!")    
+        
+        
+main()    
+    
